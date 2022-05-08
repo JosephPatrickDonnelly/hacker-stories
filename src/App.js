5b2b4b3c99a876c33293b2114bd4dc7fc79e9ad1
Joseph Patrick Donnelly <JosephPatrickDonnelly@gmail.com>
2022-05-08 16:46:23 -0700

    I removed the default Learn React code from npx create-react-app and a Hello World header

--- src/App.js
+++ src/App.js
@@ -4,21 +4,8 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload coder.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Joe Cool
-        </a>
-      </header>
+    <div>
+      <h1>Hello Joe Cool !!</h1>
     </div>
   );
 }
