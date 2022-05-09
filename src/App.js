fcae022b336340dceaaaa2366de6cb338b5aff2b
Joseph Patrick Donnelly <JosephPatrickDonnelly@gmail.com>
2022-05-08 20:34:13 -0700

    Adding some JSX to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+const title = "Joe Smiles";
+const welcome = {
+  greeting: 'Hey',
+  title: 'Joe React',
+};
 
-import logo from './logo.svg';
+function getTitle(title){
+  return title;
+}
 
 function App() {
+  
   return (
     <div>
-      <h1>Hello Joe Cool !!</h1>
+      <h1>
+        {welcome.greeting} {getTitle('Joe React')}!!!
+      </h1>
+      
+      <label htmlFor='search'>Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
