db83e039a1fdec7e5768380011a6e99068e7d065
Joseph Patrick Donnelly <JosephPatrickDonnelly@gmail.com>
2022-05-30 22:18:51 -0700

    App.js used Object Destructing in the function signatures of Search, List and Item to access all the properties needed in the child React function component.

--- src/App.js
+++ src/App.js
@@ -58,11 +58,8 @@ const App = () => {
   );
 };
 
-const Search = (props) => {
-  
- console.log('Search renders');
- const { search, onSearch } = props;
- 
+const Search = ({search, onSearch}) => {
+  console.log('Search renders');
  return (
     <div>
       <label htmlFor='search'>Search: </label>
@@ -72,27 +69,27 @@ const Search = (props) => {
   );
  };
 
-const List = (props) => {
+const List = ({ list}) => {
   console.log('List renders');
   return (
     <ul>
-        {props.list.map((item) => (
+        {list.map((item) => (
           <Item key={item.objectID} item={item} />
         ))}
       </ul>
   );
 };
         
-const Item = (props) => {
+const Item = ({item}) => {
   
   return (
-          <li key={props.item.objectID}>
+          <li key={item.objectID}>
             <span>
-              <a href={props.item.url}> {props.item.title}</a>
+              <a href={item.url}> {item.title}</a>
             </span>
-            <span> {props.item.author}</span>
-            <span> {props.item.num_comments}</span>
-            <span>{props.item.points}</span>
+            <span> {item.author}</span>
+            <span> {item.num_comments}</span>
+            <span>{item.points}</span>
           </li>
   );
 };  
